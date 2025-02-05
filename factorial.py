094804c01da314e4748d80854c1d47a7cb67c537
yash <yp024840@gmail.com>
2025-02-05 15:27:20 +0530

    my initial commit

---
+++ factorial.py
@@ -0,0 +1,12 @@
+# Input: An integer number
+num = 6
+
+# Initialize the factorial variable to 1
+factorial = 1
+
+# Calculate the factorial using a for loop
+for i in range(1, num + 1):
+    factorial *= i
+
+# Output: The factorial of the number
+print(f"The factorial of {num} is {factorial}")
